e74823a71596a241e9dae489dcf1bb9653c174b8
unknown <deepalilodhi212055@gmail.com>
2024-01-06 18:09:08 +0530

    Merge remote changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,16 +5,16 @@ import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <div className='w-full bg-black flex flex-wrap justify-evenly py-10 px-20'>
+    <div className='w-full bg-gray-600 flex flex-wrap justify-evenly py-10 px-20'>
 
-      <div className='border-r-2  pr-5'>
+      <div className='border-r-2  flex-1 flex items-center justify-center flex-col'>
         <Link to='/'>
             <img src={logo} alt="" width={180} height={180} />
         </Link>
         <p className='text-sm pt-4'>@2024 all rights reserved</p>
       </div>
 
-      <div className='border-r-2 pr-5'>
+      <div className='border-r-2 pt-2 flex-1 text-center gap-2'>
         <ul>
             <li><Link to="/about">About</Link></li>
             <li><Link to="/contact">Contact</Link></li>
@@ -23,16 +23,16 @@ const Footer = () => {
         </ul>
       </div>
 
-      <div className='border-r-2 pr-5'>
-        <h1>Follow us</h1>
+      <div className='border-r-2  flex-1 flex flex-col items-center'>
+        <h1 className='text-xl font-bold'>Follow us</h1>
         <ul>
-            <li><FaFacebook/> Facebook</li>
-            <li><FaTwitter /> Twitter</li>
-            <li><FaInstagram/> Instagram</li>
+            <li className='flex items-center justify-start gap-3 pt-1'><FaFacebook/> Facebook</li>
+            <li className='flex items-center justify-start gap-3 pt-1'><FaTwitter /> Twitter</li>
+            <li className='flex items-center justify-start gap-3 pt-1'><FaInstagram/> Instagram</li>
         </ul>
       </div>
 
-      <div className='border-r-2 pr-5'>
+      <div className='  flex-1 flex items-center justify-center pointer'>
       <a href="#subscribe">
         <h1>Subscribe to our news</h1>
       </a>
